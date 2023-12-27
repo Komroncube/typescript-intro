4028dbab36e965fb14578db1af5017bcba0878ef
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-27 20:57:28 +0500

    working with objects

---
+++ js/objects.js
@@ -0,0 +1,26 @@
+let person = { name: "Tom", age: 23 };
+person = { name: "boy", age: 32 };
+function printUser(user) {
+    if ('age' in user) {
+        console.log(user.name);
+        console.log(user.age);
+    }
+    else {
+        console.log(user.name);
+    }
+}
+let bob = { name: "Faqat ism", isMarried: true };
+function defaultUser() {
+    return { name: "Tom", age: 43 };
+}
+let defUser = defaultUser();
+function printDefaultUser({ name, age = 32 }) {
+    console.log(name);
+    console.log(age);
+}
+let jason = { name: "Tom", age: 36 };
+let bill = { name: "Bob", age: 41 };
+function getPerson() {
+    return { name: "Michele", age: 32 };
+}
+//# sourceMappingURL=objects.js.map
---
+++ objects.ts
@@ -0,0 +1,44 @@
+let person: { name: string; age?: number } = { name: "Tom", age: 23 };
+// console.log(person.age);
+
+person = {name:"boy", age:32}
+// person = {name : "Bill"}
+// if qo'shib qo'ysa bo'ladi
+// console.log(person.age);
+function printUser(user: {name:string; age?:number})
+{
+    if('age' in user)
+    {
+        console.log(user.name);
+        console.log(user.age);
+    }
+    else{
+        console.log(user.name);
+    }
+    
+}
+// printUser(person)
+
+let bob =  {name: "Faqat ism", isMarried:true}
+//printUser(bob)
+
+
+function defaultUser() : {name: string; age: number} {
+    return {name:"Tom", age:43}
+}
+let defUser = defaultUser()
+function printDefaultUser({name,age=32 }: {name:string; age?:number})
+{
+    console.log(name);
+    console.log(age);
+}    
+
+type Person = {name:string; age:number;}
+let jason : Person = {name: "Tom", age: 36};
+let bill: Person = {name: "Bob", age: 41};
+function getPerson<T extends Person|number>():T{
+
+    return {name : "Michele", age: 32} as T
+}
+
+// userlist bor shuni front endga chiqarish kerak,
