ba63a898f1d457de855ebc88ebc972faa65ee140
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-27 17:49:58 +0500

    learning types, functions etc.

---
+++ js/studyFunctions.js
@@ -0,0 +1,29 @@
+function addAnything(a, b) {
+    return a + b;
+}
+function sayHello(name, lastName = "Tompson") {
+    if (lastName)
+        console.log(`Hello ${name} ${lastName}`);
+    else {
+        console.log(`Hello ${name}`);
+    }
+}
+let operation;
+let nullnum = 32;
+function mathOperation(a, b, oper) {
+    console.log(oper(a, b));
+}
+function simpleMathOperatoin(a, b, oper) {
+    console.log(oper(a, b));
+}
+function printSentence(words) {
+    if (typeof words === "string") {
+        console.log(words);
+    }
+    else {
+        console.log(words.join(" "));
+    }
+}
+printSentence(["Язык", "программирования", "TypeScript"]);
+printSentence("Язык программирования JavaScript");
+//# sourceMappingURL=studyFunctions.js.map
---
+++ studyFunctions.ts
@@ -0,0 +1,46 @@
+function addAnything(a, b){
+    return a+b
+}
+function sayHello(name:string, lastName: string = "Tompson")
+{
+    if(lastName)
+        console.log(`Hello ${name} ${lastName}`);
+    else
+    {
+        console.log(`Hello ${name}`)
+    }
+}
+
+// console.log(addAnything(1,2));
+// console.log(addAnything("1", "2"));
+// sayHello("bob", "philips")
+let operation : (a:number, b:number) => number;
+// operation = addAnything;
+// console.log(operation(2,4));
+type Operation = (a:number, b:number) => number
+
+
+//creating own types
+type nullabel = number|null;
+let nullnum :nullabel = 32
+
+function mathOperation(a:number, b:number, oper : (a:number, b:number) => number) : void {
+    console.log(oper(a,b));
+}
+function simpleMathOperatoin(a:number, b:number, oper:Operation) : void{
+    console.log(oper(a,b));
+}
+// mathOperation(2,555, addAnything)
+// simpleMathOperatoin(48,484, addAnything)
+
+function printSentence(words: string[]|string){
+    // если words - строка
+    if (typeof words === "string") {
+      console.log(words);
+    } else {
+      // Если words - массив string[]
+      console.log(words.join(" "));
+    }
+}
+printSentence(["Язык", "программирования", "TypeScript"]);
+printSentence("Язык программирования JavaScript");
